d0ef3464c4ef05fb752dcf1001f65e02958cd07b
Sanchit Lohia <sanchitlohia27@gmail.com>
2019-10-20 13:32:34 +0530

    More examples.

--- main.go
+++ main.go
@@ -1,29 +1,9 @@
 package main
 
-import "fmt"
-
-type A interface {
-	walk()
-	talk()
-}
-type B struct {
-	name string
-}
-
-func (b B) walk() {
-	fmt.Println("Work")
-}
-func (b B) talk() {
-	fmt.Println("Talk")
-}
+import (
+	"github.com/golang-examples/articles/crawler"
+)
 
 func main() {
-	b := 1
-	fmt.Printf("%T\n", b)
-	fmt.Printf("%v\n", b)
-}
-
-func test(a A) {
-	a.walk()
-	a.talk()
+	crawler.Start()
 }
